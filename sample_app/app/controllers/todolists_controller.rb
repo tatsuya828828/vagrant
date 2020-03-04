0cad856a8d5f3d47f655c36e9d2b4883d438bd9e
小熊逹哉 <tatsuya@ogumatatsuyanoMacBook-Pro.local>
2020-03-04 16:26:12 +0900

    [finish]3-6

--- sample_app/app/controllers/todolists_controller.rb
+++ sample_app/app/controllers/todolists_controller.rb
@@ -9,9 +9,18 @@ class TodolistsController < ApplicationController
   	list = List.new(list_params)
   	# DBへ保存する
   	list.save
-  	# トップ画面へリダイレクト
-  	redirect_to '/top'
+  	# 詳細画面へリダイレクト
+  	redirect_to todolist_path(list.id)
   end
+
+  def index
+        @lists = List.all
+  end
+
+  def show
+        @list = List.find(params[:id])
+  end
+
   private
 
   def list_params
--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
   post 'todolists' => 'todolists#create'
 
   get 'todolists' => 'todolists#index'
+
+  get 'todolists/:id' => 'todolists#show',as: 'todolist'
+  # .../todolists/1や...todolists/3に該当する
 end
