cf2d25668754b01d81d1c8f88014308d80a27644
小熊逹哉 <tatsuya@ogumatatsuyanoMacBook-Pro.local>
2020-03-04 16:31:35 +0900

    [start]3-7

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
   get 'todolists' => 'todolists#index'
 
   get 'todolists/:id' => 'todolists#show',as: 'todolist'
+
+  get 'todolists/:id/edit' => 'todolists#edit', as: 'edit_todolist'
   # .../todolists/1や...todolists/3に該当する
 end
