8cdcb5cf082d82ab82cdc1a0850ea44032932906
小熊逹哉 <tatsuya@ogumatatsuyanoMacBook-Pro.local>
2020-03-11 23:22:31 +0900

    [finish]meshiterro

---
+++ meshiterro/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'db4fb839592d2247ce7fc4564fba6a601c4a5a2e5d946cd1da102aea183fa323ac7047bfbd703e7f3108be365ff6904d50c687a668b4c07c21fb76b4ba7d97e6'
